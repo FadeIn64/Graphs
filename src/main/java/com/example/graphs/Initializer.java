34d2167f49e0c6556d5fcbf197cf8628bbd4cf79
FadeIn64 <danik6052@gmail.com>
2023-10-29 17:22:06 +0400

    Test FileExtractor.java

--- src/main/java/com/example/graphs/Initializer.java
+++ src/main/java/com/example/graphs/Initializer.java
@@ -1,9 +1,18 @@
 package com.example.graphs;
 
+import com.example.graphs.graph.FileExtractor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
 
 @Component
 public class Initializer {
+
+    @SneakyThrows
     public Initializer() {
+        File test = new File("C:\\Users\\User\\Desktop\\Test.txt");
+        FileExtractor fileExtractor = new FileExtractor(test);
+        System.out.println(fileExtractor.getLines());
     }
 }
